f9f8ddc885bfa9fa17420500abdd82af59518614
Constantin Müller <nocnocdev@gmail.com>
2015-08-09 19:26:37 +0200

    Put the project under the GNU LESSER

---
+++ src/de/nocnoc/example/ee/logging/impl/controller/DateMessageUtils.java
@@ -0,0 +1,82 @@
+package de.nocnoc.example.ee.logging.impl.controller;
+
+/*
+ This file is part of InterceptorLoggingExample.
+
+ InterceptorLoggingExample is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ InterceptorLoggingExample is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+ Diese Datei ist Teil von InterceptorLoggingExample.
+
+ InterceptorLoggingExample ist Freie Software: Sie koennen sie unter den Bedingungen
+ der GNU Lesser General Public License, wie von der Free Software Foundation,
+ Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spaeteren
+ veroeffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+ InterceptorLoggingExample wird in der Hoffnung, dass es nuetzlich sein wird, aber
+ OHNE JEDE GEWAEHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ Gewaehrleistung der MARKTFAEHIGKEIT oder EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+ Siehe die GNU Lesser General Public License fuer weitere Details.
+
+ Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+ Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+import de.nocnoc.example.ee.logging.impl.logging.EntryExitLogger;
+import de.nocnoc.example.ee.logging.impl.logging.EntryExitLogging;
+import de.nocnoc.example.ee.logging.impl.logging.HasLogger;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.inject.Inject;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import static de.nocnoc.example.ee.logging.impl.logging.LogLevel.FINER;
+import static de.nocnoc.example.ee.logging.impl.logging.LogLevel.WARNING;
+
+/**
+ * Managing String Formats for Dates
+ */
+@ApplicationScoped
+public class DateMessageUtils implements HasLogger {
+
+    @Inject
+    @EntryExitLogger(value = FINER, handlerLogLevel = FINER)
+    private transient Logger logger;
+
+    @EntryExitLogging(FINER)
+    public String getSimpleDateMessage(Date date) {
+
+        date.setTime(date.getTime() + 12345);
+
+        ZonedDateTime zdt = date.toInstant().atZone(ZoneId.systemDefault());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        return formatter.format(zdt);
+    }
+
+    @EntryExitLogging(WARNING)
+    public void noReturn(Object object) {
+
+    }
+
+    @Override
+    public Logger getLogger() {
+        return this.logger;
+    }
+
+}
--- src/de/nocnoc/example/ee/logging/impl/logging/LoggerProducer.java
+++ src/de/nocnoc/example/ee/logging/impl/logging/LoggerProducer.java
@@ -1,4 +1,36 @@
-package de.nocnoc.impl.logging;
+package de.nocnoc.example.ee.logging.impl.logging;
+
+/*
+ This file is part of InterceptorLoggingExample.
+
+ InterceptorLoggingExample is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ InterceptorLoggingExample is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+ Diese Datei ist Teil von InterceptorLoggingExample.
+
+ InterceptorLoggingExample ist Freie Software: Sie koennen sie unter den Bedingungen
+ der GNU Lesser General Public License, wie von der Free Software Foundation,
+ Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spaeteren
+ veroeffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+ InterceptorLoggingExample wird in der Hoffnung, dass es nuetzlich sein wird, aber
+ OHNE JEDE GEWAEHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ Gewaehrleistung der MARKTFAEHIGKEIT oder EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+ Siehe die GNU Lesser General Public License fuer weitere Details.
+
+ Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+ Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
